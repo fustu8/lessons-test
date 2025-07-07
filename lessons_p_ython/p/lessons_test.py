3022ac1e43809a776eb1f63b16e177a2aa3eb506
fustu8 <mohsmogf47@gmail.com>
2025-07-07 05:28:06 +0400

    first commit

---
+++ lessons_p_ython/p/lessons_test.py
@@ -0,0 +1,506 @@
+# hello
+
+# print("hello this is for testing what i am learing.")
+
+# lesson 1.1
+# learing about String,Integer,Float and bool
+
+# string example
+# name = "mohammed aref"
+
+# integer example
+
+# age = 21
+
+# float example
+
+# height = 1.75
+
+# bool example
+
+# is_student = True
+
+# print variables
+
+# print("name:",name)
+# print("age:",age)
+# print("height:" , height)
+# print("is student" , is_student)
+
+# lesson 1.2 learning about list,tuple and dictionary
+
+# list example
+
+# fruits = ["banana" , "apple" , "avocado"]
+# print ("list example:" , fruits)
+
+# tuple example
+
+# coordinates = 4,3
+
+# print ("tuple example:" , coordinates)
+
+# dictionary example
+
+# person = {"name": "ahmed" , "Age" : 25 , "is_student" : True }
+
+# print ("dictionary example" , person)
+
+# Arithmetic Operators
+
+#
+
+
+# question 1
+
+# answer_1 = "Sanaa"
+
+# input_answer_1 = input("What is the capital of Yemen? ")
+
+# if input_answer_1 == answer_1.lower():
+# print("Correct")
+# else:
+#  print("Incorrect")
+
+# question 2
+
+# answer_2 = 44
+
+# input_answer_2 = int(input("what is 22 + 22 ?"))
+
+# if input_answer_2 == answer_2:
+#     print("Correct")
+# else:
+#     print("Incorrect")
+
+
+# qestion 4
+
+
+# answer_4 = ["America", "France", "Mexico"]
+
+# chioses_1 = ["America", "France", "Mexico"]
+
+# print("in which country is texas ? ")
+# print("choices: " + ", ".join(chioses_1))
+
+# input_answer_4 = input("Your answer:")
+
+# # # TODO: #make sure to add more questions and answers so i can test my knowledge and what i am learning
+
+# if input_answer_4.strip().lower() in [ans.lower() for ans in answer_4]:
+#     print("Correct")
+# else:
+#     print("Incorrect")
+
+
+# test
+# name = "Account name: "
+# name = name + "Elton"
+# name = name + " John"
+
+# print(name)
+
+# name1 = "Account name: "
+# name1 = name1 + "Mohammed"
+# name1 = name1 + " Aref"
+
+# print(name1)
+
+
+# math1 = 10
+# math1 = math1 + 4
+# math1 = math1 + 6
+
+# print(math1)
+
+
+# studentName = "name:"
+# studentName = studentName + " Mohammed"
+# studentName = studentName + " Aref"
+
+# print(studentName)
+
+
+# print ("\nMohammed account was 1000")
+
+# mohammed_b = 1000
+# mohammedItemprice = "499"
+
+# mohammed_rb = mohammed_b - 499
+
+# print("\nMohammed buyed a laptop for "  + mohammedItemprice)
+# print ("\nand left with" , int(mohammed_rb) , "account balance")
+
+# print("")
+
+
+# sales = 5
+# sales += 1
+# print(sales)
+
+# account = 1000
+# account += 500
+# account += 500
+# account += 1000
+
+# print(account)
+
+
+# ...existing code...
+
+
+# count = 0
+# while True:
+#     print("hello world")
+#     count += 1
+#     if count == 3:
+#         break
+
+# for i in range(5):
+#     add = i + 1
+#     print ("hello world")
+#     print(add)
+
+
+# ...existing code...
+
+# for i in range(2):
+#     print ("hello world")
+#     print(i)
+
+# Keep_going  = True
+
+# while Keep_going == True :
+#   print("i am learing python")
+#   Keep_going = False
+#   print("i am still learing python")
+
+#   for i in range(3): 
+#     print("i in range", i + 1)
+
+
+
+# lives = 2
+# lives = lives - 1
+# print("Lives left:", lives)
+# print("You still got a chance!")
+
+      
+# lives = lives-1
+# print("Lives left:" ,str(lives) ) 
+# print("Game over!")
+
+# for i in range (8):
+#     print("i am trying")  
+
+# ver = 80
+
+# while ver < 91 : 
+#     print("who are we ", ver)
+#     ver += 1
+
+
+# batata = 0   
+
+# while batata <= 15:
+#  print(batata)
+#  batata += 1
+ 
+
+# print ("form 0 to 4:")
+
+# for num in range (5):
+#     print(num)
+   
+# print("from 1 to 4")
+
+# number= 1
+
+# while number <= 4 :
+#     print(number)
+#     number += 1
+
+
+
+
+# for i in range(1, 6):
+#  if i == 2:
+#   continue
+ 
+#  print(i)
+
+# for x in range (1, 11):
+#  if x == 10:
+#   continue
+#  print(x)
+
+
+# shopping_list = ["apples", "bananas", "bread", "milk", "chips", "eggs"]
+
+# for item in shopping_list:
+#  if item == "chips":
+#   continue
+
+#  print(f"Don't forget to buy: {item}")
+
+
+# shop_items = ["avocado", "mint", "water bottles", "apple"]
+
+# for i in shop_items:
+#  if i == "mint:":
+#   continue
+#  print(f"don't forget to buy this items:{i}")
+
+# for i in range (1,8):
+ 
+#  if i == 5 :
+#   continue
+ 
+#  print(i)
+
+
+
+
+
+
+# password_user0 = "open_sesame"
+
+# while True:
+#   if input("Enter the password for user_0: ") == password_user0:
+#    print("Access granted!")
+#    break
+#   else:
+#    print("Incorrect password. Try again.")
+
+
+
+# password_user1 = "1233"
+
+
+# while True:
+
+#  if input("enter the password user1: ") == password_user1 :
+#     print ("correct password")
+#     break
+#  else:
+#      print("Incorrect password")
+     
+# count = 0
+# while count < 5:
+#   if count == 3:
+#     count = count + 1
+#     continue 
+#   print(count)
+#   count = count + 1
+
+#if i want it to continue |
+#                         | 
+#                         
+
+
+# count = 0
+
+# while count < 5:
+#   if count == 3:
+#    count = count + 1
+#    continue
+#   print(count)
+#   count = count + 1
+
+
+# if i want it to break 
+
+# count1 = 0 
+
+# while count1 < 5:
+#   if count1 == 3:
+#     break
+#   print (count1)
+#   count1 = count1 + 1
+
+
+
+# home_items = ["milk,", "orange juice,", "water" ]
+# personal_items = ["shaving cream,", "tooth paste,","blue pen" ]
+
+# print("home items:" , home_items[0],home_items[1],home_items[2])
+# print("\npersonal items:", personal_items[0],personal_items[1],personal_items[2])
+
+# print("                                                              ")
+
+
+
+# for i in range (4):
+#     print ("mohammed aref:testing for range loops ")
+
+
+# name = "Mohammed aref testing while loops "
+
+# count = 0
+
+# while count < 4 :
+#     print(name)
+#     count += 1
+
+
+# temp = [23,12,12,2]
+# temp[2] = 0
+# temp[1] = 40
+# temp[0] = 30
+
+# print(temp)
+#lists
+# text = ["water", "avocado" , "milk"]
+# #to add alamunium to the last element
+# text.append("alamunium")
+# #to remove water 
+# text.remove(text[0])
+# #to add "200" on the first element which is zero
+# text.insert(0,"200")
+# #to remove the last element which is alamuium the element that we added to the last using append 
+# text.pop()
+# #we can also chose which element we can pob and i choose to remove 2nd element which is avocado 
+# text.pop(1)
+# #we can also store the removed value in a variable 
+# removed_test =text.pop(0)
+# #print the removed value 
+# print (removed_test)
+# #move milk in a variable 
+# move_milk = text.pop(0)
+# #print the moved variable 
+# print(move_milk)
+# #add any thing to the list 
+# text.insert(0,"there its nothing inside")
+
+# #show the user the text 
+
+# print(text)
+
+# example1 = "hello"
+# example2 = "world"
+# example3 = "i am testing"
+# all_examples =[example1,example2,example3]
+
+# print(all_examples[2], all_examples[0], all_examples[1] )
+
+
+# cake = ["chocolate","strawberry", "pistachio"] 
+# cake[1] = "apple"
+# print(cake)
+
+
+
+# num = [1,2,3]
+# num.append(4)
+# print(num)
+           
+# final_scores = [12,14,15,16]
+
+# for scores in final_scores :
+
+#     print(scores)
+    
+
+# names = ["ahmad", "mohammed", "abdullah"]
+
+# for i in names : 
+#     print(i)
+
+
+# names2 = ["hello", "world", "i", "love", "coding"] 
+
+# print(len(names2))
+
+# chores = ["clean", "do study", "bring home items"]
+# chores = []
+# if len(chores) > 0 : 
+#     print ("i still have a lot of work to do ")
+# else: 
+#     print("i am done")    
+
+# ver = [1.2,1.3,1.4]
+# print(ver[0], ver[1], ver[2], ver[2] + 0.1)
+
+# for i in ver :
+#    print(i)
+
+
+# value = 0
+
+# print(value)
+
+# user_input = input("how man you want to add to the value ")
+
+# print(int(user_input) + int(value))
+
+
+
+# apples = 20
+
+# ask_cus = input("how many apple do you want? ")
+
+# requested_apples = int(ask_cus)
+
+# if requested_apples <= apples:
+    
+#  print(f"here is your {ask_cus} apples the you requested")
+
+# else: 
+#   print("we don't have that much of apples in store ")
+
+
+
+
+# mangos = 50 
+
+# while mangos > 0 :
+
+#     print(f"welcome to the store we have {mangos} mangos in stock")
+
+#     user_input_str = input("how many mangos would you like to buy ,(Type 'exit' if you want to quit  ) ")
+
+#     if user_input_str.lower() == 'exit' : 
+#         print("Good bye :( we hope to see you next time ")
+#         break
+
+#     try:
+
+#         user_into_int = int(user_input_str)
+#     except ValueError:
+#         print("Invalid input. please enter a number.")
+#         continue
+
+#     if user_into_int <= 0 :
+#         print("You must at least request one mango")
+#         continue
+
+#     if user_into_int <= mangos :
+#         print(f"here is your {user_into_int} apples")
+
+#         mangos = mangos - user_into_int
+#     else:
+        
+#         print(f"we don't have that many. we only have {mangos} left.")
+
+# print("\n--- sorry we are all out of apples--- ")
+
+
+
+    
+
+for i in range(9):
+  print("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
